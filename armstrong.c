01e28ed8a356ff83d8913ac376b5e8e25139abec
Shivam Gupta <shivam16121993@gmail.com>
2020-02-07 21:55:44 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,48 +1,22 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
+#include<math.h>
 int main()
-{
-    
-int i,a,b,c=0,d;
-    
-printf("Enter the number to check if it is armstrong or not=");
-    
-scanf("%d",&a);
-    
-i=a;
-    
-while(a!=0)
-    {
-        
-b=a%10;
-        
-a=a/10;
-        
-d=b*b*b;
-        
-c+=d;    
-    
-        
-    
-}
-    
-if (i==c)
-    
-{
-        printf("number is armstrong");
-        
-    
-}else
-    {
-        
-printf("number is not armstrong");
-    
-}
-    
-	
-
-return 0;*q--, *--q, --(*q), --*q,
-(*q)--, *(q--), *(--q)
-
-}
+{int a,i,b,amsum=0,d;
+	int c[50];
+	printf("enter the number whose armstrong number has to be printed\n");
+	scanf("%d",&a);
+	d=a;
+	for(i=0;a>0;i++)
+		{c[i]=a%10;
+		a=a/10;
+		}
+	b=i;
+	for(i=0;i<b;i++)
+	amsum=amsum+pow(c[i],b);
+	printf("armstrong number is %d\n",amsum);
+	if(d==amsum)
+	printf("%d is an armstrong number\n",d);
+	else
+	printf("%d is not an armstrong number\n",d);
+	return 0;
+	}
